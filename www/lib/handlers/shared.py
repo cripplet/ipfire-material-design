d2c4ce0948e4337e52075f3514e6f4ff067acebe
Minke Zhang <minke.zhang@google.com>
2019-07-07 19:01:31 -0700

    Fix path indexing address resolution error

--- www/lib/handlers/shared.py
+++ www/lib/handlers/shared.py
@@ -7,10 +7,13 @@ from lib.handlers import not_found_handler
 
 def _traverse_dict(d, path):
   d = dict(d)
-  if not path:
-    return d
   for p in path.split('/'):
-    d = d[p]
+    if not p:
+      break
+    try:
+      d = d[p]
+    except TypeError:
+      d = d[int(p)]
   return d
 
 
@@ -21,6 +24,6 @@ def config_handler(path, config, handler):
           http.HTTPStatus.OK,
           {'Content-type': 'text/plain'},
           json.dumps(_traverse_dict(config, path), indent=4))
-    except KeyError:
+    except (KeyError, IndexError):
       return not_found_handler.not_found_handler(handler)
   return method_not_allowed_handler.method_not_allowed_handler(handler)
