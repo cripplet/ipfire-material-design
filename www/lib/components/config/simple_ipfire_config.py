85525106dd94f5925a12325229acb00cb7dd71cf
Minke Zhang <minke.zhang@gmail.com>
2019-07-12 03:45:05 -0700

    Fix view bug handling non-existent settings file

--- www/lib/components/config/simple_ipfire_config.py
+++ www/lib/components/config/simple_ipfire_config.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import AnyStr
 
 from lib.components import shared
@@ -6,16 +7,17 @@ from lib.components.config import shared as shared_config
 
 
 def get_simple_ipfire_config(component: AnyStr) -> shared.ConfigType:
-  if component not in set([c.value for c in shared.Component]):
+  with open('config/ipfire_shim.json') as fp:
+    ipfire_root = json.loads(fp.read())['ipfire_root']
+  fn = '{ipfire_root}/{component}/settings'.format(
+      ipfire_root=ipfire_root,
+      component=component)
+
+  if component not in set(
+      [c.value for c in shared.Component]) or not os.path.exists(fn):
     raise KeyError(
         'Cannot find specified component \'{c}\''.format(
             c=component))
 
-  with open('config/ipfire_shim.json') as fp:
-    ipfire_root = json.loads(fp.read())['ipfire_root']
-
-  with open(
-      '{ipfire_root}/{component}/settings'.format(
-          ipfire_root=ipfire_root,
-          component=component)) as fp:
+  with open(fn) as fp:
     return shared_config.IPFireConfigShim().FromEngine(fp.read())
